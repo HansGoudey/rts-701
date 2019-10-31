c06db3269855db05e27ffc42ebd99497731f192a
Hans Goudey <h.goudey@me.com>
2019-10-30 21:08:44 -0400

    Trying to get terrain intersection to work, node not found

--- RTS-701-Project/Player.gd
+++ RTS-701-Project/Player.gd
@@ -142,9 +142,12 @@ func project_mouse_to_terrain_plane() -> Vector3:
 	var to:Vector3 = from + camera.project_ray_normal(mouse_position) * 1000
 
 	# TODO: Use a raycast to intersect with terrain instead of the y = 0 plane
-	var navigation_node:Navigation = get_node("../../Map/Navigation")
-	return navigation_node.get_closest_point_to_segment(from, to)
-#	return isect_line_plane_v3(to, from, Vector3(0, 0, 0), Vector3(0, 1, 0))
+	var navigation_node:Navigation = find_node("Navigation", true, false)
+	if navigation_node:
+		return navigation_node.get_closest_point_to_segment(from, to)
+	else:
+		print("Didn't find navigation node: intersecting with y = 0")
+		return isect_line_plane_v3(to, from, Vector3(0, 0, 0), Vector3(0, 1, 0))
 
 func start_box_select():
 	# Raycast to the terrain plane (y = 0) to get the starting location
