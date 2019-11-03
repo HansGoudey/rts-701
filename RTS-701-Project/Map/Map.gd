2b79dc980553967705a1f743844af767a0f392e6
Roger-Dai <roger.dai1997@gmail.com>
2019-11-03 18:49:33 -0500

    basic resource placement working for one type

--- RTS-701-Project/Map/Map.gd
+++ RTS-701-Project/Map/Map.gd
@@ -3,6 +3,9 @@ extends Spatial
 var navigation:Navigation = null
 var navmesh_id:int = 0
 
+var num_of_resources: int = 10
+var rng = RandomNumberGenerator.new()
+
 func _ready():
 	# Add the terrain
 	var terrain_file = preload("res://Map/SimpleTerrain.glb")
@@ -20,3 +23,17 @@ func _ready():
 	navmesh_id = navigation.navmesh_add(navigation_mesh, Transform.IDENTITY)
 	navigation_mesh_instance.set_enabled(true)
 	navigation.add_child(navigation_mesh_instance)
+	
+	randomly_place_resources()
+
+func randomly_place_resources():
+	var navigation_node:Navigation = get_node("/root/Main/Game/Map/Navigation")
+	for i in range(num_of_resources):
+		var x = rng.randi_range(-45, 45)
+		var z = rng.randi_range(-45, 45)
+		var location = navigation_node.get_closest_point_to_segment(Vector3(x,0,z), Vector3(x, 1000, z))
+		var resource_scene = load("res://Resources/ResourceBasic.tscn")
+		var resource_node = resource_scene.instance()
+		resource_node.translate(location)
+		add_child(resource_node)
+		
