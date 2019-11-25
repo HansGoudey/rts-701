a147fa705b0b5c07bc0ecb28cada9b568367057d
antho <aturcios@middlebury.edu>
2019-11-25 10:09:37 -0500

    unit can navigate on 2D plane

--- RTS-701-Project/Units/Unit.gd
+++ RTS-701-Project/Units/Unit.gd
@@ -13,7 +13,7 @@ var navmesh_id:int = 0
 const NAVIGATION_RECALCULATION_FREQUENCY:float = 1.0 # Seconds
 var navigation_recalculation_timer:Timer = null
 
-var navigation_processed: bool = false
+var find_path: bool = true
 var initial_pos = Vector3() # initial position of the unit
 var target_location:Vector3 = Vector3(0, 0, 0) # Target Location
 var path = PoolVector3Array()
@@ -38,7 +38,7 @@ func _ready():
 
 func _physics_process(delta: float) -> void:
 	process_current_order(delta)
-
+	
 func process_current_order(delta:float) -> void:
 	if orders.size() == 0:
 		# Default behaviour without player added orders
@@ -52,11 +52,13 @@ func process_current_order(delta:float) -> void:
 	var order_type:int = get_order_type()
 	if order_type == ORDER_NAVIGATION_POSITION or order_type == ORDER_NAVIGATION_NODE:
 		# Process current navigation goal (recalculate at a frequency)
+		if path.size() == 0:
+			find_path = true
 		process_navigation(delta)
 
 		# If navigation is complete, pop it from the queue
-		
-		pass
+		if path.size() == 0:
+			orders.pop_front()
 	elif order_type == ORDER_TYPE_ATTACK:
 		# Do navigation the same as above
 
@@ -69,31 +71,31 @@ func process_current_order(delta:float) -> void:
 		pass
 		
 func process_navigation(delta:float) -> void:
-	if not navigation_processed:
+	if find_path:
 		initial_pos = get_translation()
 		target_location = get_navigation_target_position()
-		var nav = get_node('/root/Main/Game/Map/Navigation') 
-		if nav:
-			print('navigation found')
-		path = nav.get_simple_path(
-			initial_pos, 
-			# do not let units pile up on eachother
-			target_location+Vector3(randi()%100, randi()%100, randi()%100) 
-		)
-		
-		navigation_processed = true
+		var nav = get_node('/root/Main/Game/Map/Navigation')
+		path = nav.get_simple_path(initial_pos, target_location)
+		find_path = false
 	if path.size() > 0:
-		print('path size still there')
 		move_unit(initial_pos, path[0], delta)
-	else:
-		navigation_processed = false
 
 func get_z() -> float:
 	var mouse_position:Vector2 = get_viewport().get_mouse_position()
 	var from:Vector3 = camera.project_ray_origin(mouse_position)
 	var to:Vector3 = from + camera.project_ray_normal(mouse_position) * 1000
-	return get_node('/root/Main/Game/Map/Navigation').get_closest_point_to_segment(from, to).z
-	
+	return isect_line_plane_v3(to, from, Vector3(0, 0, 0), Vector3(0, 1, 0)).z
+
+func isect_line_plane_v3(l1:Vector3, l2:Vector3, plane_co:Vector3, plane_no:Vector3) -> Vector3:
+	var u:Vector3 = l2 - l1
+	var h:Vector3 = l1 - plane_co
+	var dot:float = plane_no.dot(u)
+
+	if abs(dot) > 0.0000001:
+		var lambda:float = - (plane_no.dot(h) / dot)
+		return l1 + u * lambda
+	else:
+		return Vector3(0, 0, 0)
 #	var u:Vector3 = l2 - l1
 #	var h:Vector3 = l1 - plane_co
 #	var dot:float = plane_no.dot(u)
@@ -108,14 +110,11 @@ func move_unit(from: Vector3, to: Vector3, delta:float):
 	# get unit direction from one vector to the other
 	var v = (to-from).normalized()
 	v *= delta * SPEED # scale for how much to move
-	var next_pos = initial_pos + v
-	#if next_pos.distance_squared_to(from) < 3:
-	path.remove(0)
-	initial_pos = next_pos
-	translation.x = initial_pos.x
-	translation.y = initial_pos.y
-	translation.z = get_z()
-		
+	translation += v # need to account for change in the z
+	if translation.distance_squared_to(to) < 9: # this constant
+		path.remove(0)
+		initial_pos = translation
+
 func get_navigation_target_position():
 	var order_type:int = get_order_type()
 	if order_type == ORDER_NAVIGATION_POSITION:
