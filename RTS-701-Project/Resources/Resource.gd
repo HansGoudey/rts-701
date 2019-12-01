d80fb6086fa4d83779153de0918105121713cc31
Roger-Dai <roger.dai1997@gmail.com>
2019-12-01 00:33:12 -0500

    now all three types of resources are initialized

--- RTS-701-Project/Resources/Resource.gd
+++ RTS-701-Project/Resources/Resource.gd
@@ -3,6 +3,7 @@ extends Spatial
 class_name MapResource
 
 var health:int = 100
+var resource_type:int
 
 enum {RESOURCE0 = 0, RESOURCE1 = 1, RESOURCE2 = 2}
 
@@ -22,6 +23,7 @@ func load_resource(type:int) -> void:
 		resource_scene = load("res://Resources/3.glb")
 	var resource_mesh = resource_scene.instance()
 	add_child(resource_mesh)
+	self.resource_type = type
 
 # Returns an amount harvested from the resource
 func harvest(damage:int) -> int:
